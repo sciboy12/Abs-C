d6be8b92517f50f1839c21648304db04ac972ec6
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2019-08-16 09:31:01 -0400

    Grab touchpad

--- abs-c.c
+++ abs-c.c
@@ -21,52 +21,53 @@ char *tpad = "Touchpad", *tpad1 = "TouchPad";
 ndevs = scandir("/dev/input/", &namelist, NULL, alphasort);
 
 for (i = 0; i < ndevs; i++) {
-	snprintf(path, sizeof path, "%s%s", "/dev/input/", namelist[i]->d_name);
-	fd = open(path, O_RDONLY);
-	ioctl(fd, EVIOCGNAME(sizeof(name)), name);
-	if (strstr(name,tpad) != NULL | strstr(name,tpad1) != NULL){found = 1; break;}
-	}
-	if (found != 1){printf("Touchpad not found\n"); exit(0);};
+    snprintf(path, sizeof path, "%s%s", "/dev/input/", namelist[i]->d_name);
+    fd = open(path, O_RDONLY);
+    ioctl(fd, EVIOCGNAME(sizeof(name)), name);
+    if (strstr(name,tpad) != NULL | strstr(name,tpad1) != NULL){found = 1; break;}
+    }
+    if (found != 1){printf("Touchpad not found\n"); exit(0);};
 
 struct input_event ev;
 int tmin_x,tmax_x,tmin_y,tmax_y,x,y,x_old,y_old,int_y,int_x;
-float ratio,y_min_scaled,x_total,y_total;
-// Detect min and max touchpad values
-int abs[0];
+// Min and max touchpad values
+int abs[1];
 ioctl(fd, EVIOCGABS(ABS_X), abs);
 tmin_x = abs[1];
 tmax_x = abs[2];
 ioctl(fd, EVIOCGABS(ABS_Y), abs);
 tmin_y = abs[1];
 tmax_y = abs[2];
-// Ratio calculation algorithm
-x_total = tmin_x - tmax_x;
-y_total = tmin_y - tmax_y;
-ratio = y_total / x_total;
-y_min_scaled = tmin_y * (ratio * 2);
-tmin_y = y_min_scaled;
-// Get display resolution
+// Display resolution
 Display *disp;
 Screen *scr;
 Window root_window;
 disp = XOpenDisplay(0);
 root_window = XRootWindow(disp, 0);
 scr = ScreenOfDisplay(disp, 0);
-
+// Aspect ratio
+float sx, sy, tx, newty;
+tx = tmax_x;
+sx=scr->width;
+sy=scr->height;
+newty=(sy / sx) * tx;
+tmax_y=(int)newty;
+// Grab device
+ioctl(fd, EVIOCGRAB);
 printf("Press Ctrl-C to quit\n");
 while(1)
 {
-	read(fd,&ev, sizeof(ev));
-	if( ev.code == 0 & ev.value != 0 ) { x = ev.value; }
-	if( ev.code == 1 ) { y = ev.value; }
-	if(x != x_old | y != y_old){
-	int_x = ( x - tmin_x ) * scr->width / ( tmax_x - tmin_x );
-	int_y = ( y - tmin_y ) * scr->height / ( tmax_y - tmin_y );
-	XWarpPointer(disp, None, root_window, 0, 0, 0, 0, int_x, int_y);
-	XFlush(disp);
-	}
-	if( ev.code == 0 & ev.value != 0 ) { x_old = ev.value; }
-	if( ev.code == 1 ) { y_old = ev.value; }
+    read(fd,&ev, sizeof(ev));
+    if( ev.code == 0 & ev.value != 0 ) { x = ev.value; }
+    if( ev.code == 1 ) { y = ev.value; }
+    if(x != x_old | y != y_old){
+    int_x = ( x - tmin_x ) * scr->width / ( tmax_x - tmin_x );
+    int_y = ( y - tmin_y ) * scr->height / ( tmax_y - tmin_y );
+    XWarpPointer(disp, None, root_window, 0, 0, 0, 0, int_x, int_y);
+    XFlush(disp);
+    }
+    if( ev.code == 0 & ev.value != 0 ) { x_old = ev.value; }
+    if( ev.code == 1 ) { y_old = ev.value; }
 }
 
 }
